15502bdec3256e72f8ce2728ac4e84c170073338
Nicholas Lim <nicknitewolf@lineageos.org>
2025-05-03 17:12:13 +0800

    nextcloud: Migrate to aio

--- nextcloud/update.sh
+++ nextcloud/update.sh
@@ -5,3 +5,5 @@ docker compose up -d
 docker compose exec -u www-data app php occ db:add-missing-columns
 docker compose exec -u www-data app php occ db:add-missing-indices
 docker compose exec -u www-data app php occ db:add-missing-primary-keys
+docker compose exec -u www-data app php occ maintenance:repair --include-expensive
+
