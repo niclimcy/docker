8ac94e1080d70415bffb53fddf68651667f94c18
Nicholas Lim <nicknitewolf@lineageos.org>
2025-08-11 13:30:10 +0800

    nextcloud: Fix some AIO configs

--- nextcloud/update.sh
+++
@@ -1,9 +0,0 @@
-docker compose pull
-
-docker compose up -d
-
-docker compose exec -u www-data app php occ db:add-missing-columns
-docker compose exec -u www-data app php occ db:add-missing-indices
-docker compose exec -u www-data app php occ db:add-missing-primary-keys
-docker compose exec -u www-data app php occ maintenance:repair --include-expensive
-
